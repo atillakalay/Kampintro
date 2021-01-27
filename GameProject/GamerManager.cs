7643f3ac297bf2873ba4a93f1d33cd7274bb5bdd
atilla kalay <atilla.kalayy@gmail.com>
2021-01-28 00:18:57 +0300

    Ödev Simülasyonu

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace GameProject
+{
+    //MikroService
+    class GamerManager : IGamerService
+    {
+        private IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayıt Olundu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama Başarısız , Kayıt Başarısız");
+            }
+            
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStatteUserValidationManager());
+            gamerManager.Add(new Gamer{Id = 1,BirthYear = 1999,FirstName = "Atilla",IdentityNumber = 123456,LastName = "Kalay"});
+        }
+    }
+}
